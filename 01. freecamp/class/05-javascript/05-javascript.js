b49de0fc4c690404c4d6d1edf279d93090cb7334
93Sangjunlee <fhuice@gmail.com>
2022-12-28 20:22:07 +0900

    데이터 타입과 연산자부터 다시 보자

---
+++ 01. freecamp/class/05-javascript/05-javascript.js
@@ -0,0 +1,30 @@
+if(1+1 === 2) {
+    console.log("정답임")
+} else {
+console.log("틀렸음")
+}
+// VM434:2 정답임
+// undefined
+// 3
+// 3
+if(!true) {
+    console.log("정답임")
+} else {
+console.log("틀렸음")
+}
+// VM488:4 틀렸음
+// undefined
+if(true) {
+    console.log("정답임")
+} else {
+console.log("틀렸음")
+}
+// VM501:2 정답임
+// undefined
+if(0) {
+    console.log("정답임")
+} else {
+console.log("틀렸음")
+}
+// VM516:4 틀렸음
+// undefined
